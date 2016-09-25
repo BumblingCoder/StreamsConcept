b1a234e7ea6815fdc68c1ae39b806d18caac8d6b
BumblingCoder <azrael.wolfe@gmail.com>
2016-09-25 14:42:01 -0400

    Allow unsigned as well as signed ints

--- intformatter.h
+++ intformatter.h
@@ -5,7 +5,7 @@
 class IntFormatter
 {
 public:
-    template< typename T > requires std::is_signed<T>()()
+    template< typename T>
     IntFormatter(T i);
     const char* streamable_string() const {return buffer; }
 private:
@@ -16,3 +16,7 @@ template<> IntFormatter::IntFormatter<short int>( short int i ) { snprintf( buff
 template<> IntFormatter::IntFormatter<int>( int i ) { snprintf( buffer, 30, "%i", i ); }
 template<> IntFormatter::IntFormatter<long int>( long int i ) { snprintf( buffer, 30, "%li", i ); }
 template<> IntFormatter::IntFormatter<long long int>( long long int i ) { snprintf( buffer, 30, "%lli", i ); }
+template<> IntFormatter::IntFormatter<short unsigned int>( short unsigned int i ) { snprintf( buffer, 30, "%hu", i ); }
+template<> IntFormatter::IntFormatter<unsigned int>( unsigned int i ) { snprintf( buffer, 30, "%u", i ); }
+template<> IntFormatter::IntFormatter<long unsigned int>( long unsigned int i ) { snprintf( buffer, 30, "%lu", i ); }
+template<> IntFormatter::IntFormatter<long long unsigned int>( long long unsigned int i ) { snprintf( buffer, 30, "%llu", i ); }
--- main.cpp
+++ main.cpp
@@ -18,7 +18,11 @@ int main()
                                             short int, IntFormatter,
                                             int, IntFormatter,
                                             long int, IntFormatter,
-                                            long long int, IntFormatter>> stream;
+                                            long long int, IntFormatter,
+                                            unsigned short int, IntFormatter,
+                                            unsigned int, IntFormatter,
+                                            unsigned long int, IntFormatter,
+                                            unsigned long long int, IntFormatter>> stream;
     stream << 4.5f <<" float\n";
     stream << 4.5 <<" double\n";
     stream << "str"s <<" string\n";
@@ -26,5 +30,9 @@ int main()
     stream << -5 <<" int\n";
     stream << static_cast<long>(-6) <<" long\n";
     stream << static_cast<long long>(-7) <<" long long\n";
+    stream << static_cast<unsigned short>( 1 ) << " unsigned short\n";
+    stream << 2u << " unsigned int \n";
+    stream << static_cast<unsigned long>( 3 ) << " unsigned long\n";
+    stream << static_cast<unsigned long long>( 4 ) << " unsigned long long\n";
     return 0;
 }
